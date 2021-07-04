4a38a1d9ec9af0a1265245452dc0eb781f989a80
Wesley-Ryan <im.wesleyryan@gmail.com>
2021-07-04 11:06:32 -0700

    loginform added

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,14 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+import LoginForm from "../views/LoginForm.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
   {
     path: "/",
-    name: "Home",
-    component: Home,
+    name: "Login",
+    component: LoginForm,
   },
   {
     path: "/about",
