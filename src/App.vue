104e1e1c8d09e5f29a5f9cde8e417d2b95e75135
Wesley-Ryan <im.wesleyryan@gmail.com>
2021-07-04 15:38:07 -0700

    added app bar

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,40 @@
 <template>
   <v-app>
-    <v-main>
-      <router-view />
-    </v-main>
+    <v-app-bar id="nav-bar" dense dark fixed height="80px">
+      <v-app-bar-nav-icon>
+        <v-img
+          :src="require('./assets/logoOnly.png')"
+          contain
+          height="90"
+          id="img-logo"
+        />
+      </v-app-bar-nav-icon>
+
+      <v-spacer></v-spacer>
+
+      <v-menu left bottom>
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn icon v-bind="attrs" v-on="on">
+            <v-icon>mdi-dots-vertical</v-icon>
+          </v-btn>
+        </template>
+
+        <v-list>
+          <v-list-item
+            v-for="link in links"
+            :key="link.id"
+            @click="
+              () => {
+                link.action;
+              }
+            "
+          >
+            <v-list-item-title> {{ link.name }}</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+    </v-app-bar>
+    <router-view />
   </v-app>
 </template>
 
@@ -11,7 +43,19 @@ export default {
   name: "App",
 
   data: () => ({
-    //
+    links: [
+      { id: 1, name: "Account", action: console.log("Account") },
+      { id: 2, name: "Logout", action: console.log("Logout") },
+    ],
   }),
 };
 </script>
+
+<style>
+#nav-bar {
+  background: #4a5680;
+}
+#img-logo {
+  margin: 25px 5px 0 15px;
+}
+</style>
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,6 +1,18 @@
 <template>
-  <v-container fluid class="mx-auto mt-8" id="form-card">
-    <h2>Dashboard</h2>
+  <v-container fluid class="fluid" id="container">
+    <v-row justify="center" align="center" class="row">
+      <v-col style="height: 100vh" id="profile-tab">
+        <v-row justify="center" align="center">
+          <v-col>
+            <v-avatar color="primary" rounded size="64"> WW </v-avatar>
+            <p>kl;dskdl;dkasl;kdl;aksdfkas</p>
+          </v-col>
+        </v-row>
+      </v-col>
+      <v-col class="text-center" style="height: 100vh">
+        <h1>Welcome Username</h1>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
@@ -8,7 +20,27 @@
 export default {
   name: "Dashboard",
   data: () => ({
-    methods: {},
+    messages: [
+      {
+        avatar: "https://avatars0.githubusercontent.com/u/9064066?v=4&s=460",
+        name: "John Leider",
+        title: "Welcome to Vuetify!",
+        excerpt: "Thank you for joining our community...",
+        new: true,
+      },
+    ],
+    lorem:
+      "Lorem ipsum dolor sit amet, at aliquam vivendum vel, everti delicatissimi cu eos. Dico iuvaret debitis mel an, et cum zril menandri. Eum in consul legimus accusam. Ea dico abhorreant duo, quo illum minimum incorrupte no, nostro voluptaria sea eu. Suas eligendi ius at, at nemore equidem est. Sed in error hendrerit, in consul constituam cum.",
   }),
 };
 </script>
+
+<style scoped>
+#container {
+  margin-top: 90px;
+}
+#profile-tab {
+  max-width: 400px;
+  border-right: 1px solid grey;
+}
+</style>
